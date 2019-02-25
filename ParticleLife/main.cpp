7e5a91719c71469cff36e982d13f5b13d8c246f5
emerickpoulin31415 <emerickpoulin31415@gmail.com>
2019-02-24 21:13:40 -0500

    SFML Boilerplate

--- ParticleLife/main.cpp
+++ ParticleLife/main.cpp
@@ -1,7 +1,31 @@
-
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	const unsigned int width = 512;
+	const unsigned int height = 512;
+	sf::RenderWindow window(sf::VideoMode(width, height), "Particle Life");
+	window.setFramerateLimit(60);
+
+
+	sf::Event evnt;
+	while (window.isOpen())
+	{
+		while (window.pollEvent(evnt))
+		{
+			switch (evnt.type)
+			{
+			case sf::Event::Closed: // Window got closed
+				window.close();
+				break;
+			default:
+				break;
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 
 	return 0;
 }
